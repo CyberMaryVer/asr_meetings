500006d3d221ebaa92e810f477e347cec32c9317
CyberMaryVer <maria.s.startseva@gmail.com>
2021-10-16 23:50:30 +0300

    editor added

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ from silero_punctuation import apply_punkt_to_text
 from spacy_formatter import format_for_streamlit
 from get_weights import check_and_load
 
-
 # SETTINGS
-model_settings = {"Vosk": {"MICROSERVICE": "0.0.0.0", "ENABLED": "True"},
+st.set_page_config(page_title="web-app", page_icon=":vhs:", layout="wide")
+
+model_settings = {"Vosk_SM": {"MICROSERVICE": "0.0.0.0", "ENABLED": "True"},
+                  "Vosk_LG": {"MICROSERVICE": "0.0.0.0", "ENABLED": "False"},
                   "🤗": {"MICROSERVICE": "0.0.0.0", "ENABLED": "False"},
                   "Nemo": {"MICROSERVICE": "0.0.0.0", "ENABLED": "False"}}
 
@@ -21,119 +23,55 @@ def show_img(img_path, width=300):
     st.image(img_to_show, width=width)
 
 
-# def get_counter_code(counter, level=3):
-#     zeros = "0" * (level - len(str(counter)))
-#     code = f"{zeros}{counter}"
-#     return code
-#
-#
-# def clear_folder(folder):
-#     # clear the folder to avoid storage overload
-#     for filename in os.listdir(folder):
-#         file_path = os.path.join(folder, filename)
-#         try:
-#             if os.path.isfile(file_path) or os.path.islink(file_path):
-#                 os.unlink(file_path)
-#         except Exception as e:
-#             print('Failed to delete %s. Reason: %s' % (file_path, e))
-#
-#
-# def split_on_chunks(sound, folder="audio"):
-#     from pydub.silence import split_on_silence, detect_silence
-#     dBFS = sound.dBFS
-#     # sound = AudioSegment.from_file("/path/to/file.mp3", format="mp3")
-#     silence = detect_silence(sound, min_silence_len=1000, silence_thresh=dBFS - 16)
-#     silence = [((start / 1000), (stop / 1000)) for start, stop in silence]  # in sec
-#     print(silence)
-#
-#     chunks = split_on_silence(
-#         sound,
-#
-#         # split on silences longer than 1000ms (1 sec)
-#         min_silence_len=1000,
-#
-#         # anything under -16 dBFS is considered silence
-#         silence_thresh=dBFS-16,
-#
-#         # keep 200 ms of leading/trailing silence
-#         keep_silence=100
-#     )
-#     for idx, chunk in enumerate(chunks[1:]):
-#         code = get_counter_code(idx)
-#         chunk_name = f"chunk_{code}.wav"
-#         save_path = Path(folder) / chunk_name
-#         chunk.export(save_path, format="wav")
-#         # print(f"chunk_{code} is saved")
-#
-#     return chunks
-#
-#
-# @st.cache()
-# def save_audio(file, folder="audio"):
-#     os.makedirs(folder, exist_ok=True)
-#     datetoday = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-#
-#     # clear the folder to avoid storage overload
-#     clear_folder(folder)
-#
-#     try:
-#         with open("log.txt", "a") as f:
-#             f.write(f"{file.name} - {file.size} - {datetoday};\n")
-#     except Exception as e:
-#         print(f"{type(e)}: {e}")
-#
-#     if file.name.endswith("wav"):
-#         audio = pydub.AudioSegment.from_wav(file)
-#     elif file.name.endswith("ogg"):
-#         audio = pydub.AudioSegment.from_ogg(file)
-#     elif file.name.endswith('mp3'):
-#         audio = pydub.AudioSegment.from_mp3(file)
-#     elif file.name.endswith('m4a'):
-#         audio = pydub.AudioSegment.from_file(file, "m4a")
-#     elif file.name.endswith("aac"):
-#         audio = pydub.AudioSegment.from_file(file, "aac")
-#     else:
-#         return 1
-#
-#     audio = audio.set_channels(1)
-#     audio = audio.set_frame_rate(16000)
-#     split_on_chunks(audio, folder=folder)
-#     # for chunk in chunks:
-#     #     save_path = Path(folder) / (chunk.chunkname + ".wav")
-#     #     audio.export(save_path, format="wav")
-#     # save_path = Path(folder) / (file.name + ".wav")
-#     # audio.export(save_path, format="wav")
-#
-#     return 0
+@st.cache
+def get_weights():
+    check_and_load()
+
+
+def get_edited_text():
+    # get data
+    if "data" not in st.session_state.keys():
+        return None
+    else:
+        return st.session_state["data"]
+
+
+def save_edited_text():
+    # save data
+    if "editor" in st.session_state.keys():
+        if "data" not in st.session_state.keys():
+            st.session_state.update({"data": st.session_state["editor"]})
+        else:
+            st.session_state["data"] = st.session_state["editor"]
 
 
 def main():
     # side_img = Image.open("images/logo_red.png")
     with st.sidebar:
-        show_img("images/logo_red.png")
+        show_img("images/logo_red.png", width=250)
 
     st.sidebar.subheader("Menu")
-    website_menu = st.sidebar.selectbox(options=("Главная", "Документация проекта", "Команда", "Оставить отзыв",),
+    website_menu = st.sidebar.selectbox(options=("🎃 Главная 🎃", "Документация проекта", "Команда", "Оставить отзыв",),
                                         label="Page",
                                         key="0")
     st.set_option('deprecation.showfileUploaderEncoding', False)
 
-    if website_menu == "Главная":
+    if website_menu == "🎃 Главная 🎃":
+        editor = st.sidebar.checkbox("Редактор")
+        st.sidebar.write("- - - - - - -")
+        st.sidebar.subheader("Настройки")
+        test = st.sidebar.checkbox("Использовать тестовый файл")
         num = None
-        if st.sidebar.checkbox("Debug mode"):
+        if st.sidebar.checkbox("Использовать ASR для фрагмента"):
             num = st.sidebar.number_input("chunks", min_value=10, max_value=100, step=10)
-        st.sidebar.subheader("Model")
-        model = st.sidebar.selectbox("API", ("Vosk", "🤗", "Nemo"))
-        st.sidebar.subheader("Settings")
+        model = st.sidebar.selectbox("API", ("Vosk_SM", "Vosk_LG", "🤗", "Nemo"))
         st.sidebar.write(model_settings[model])
-        st.markdown("## Аудио файл")
-        test = st.checkbox("Использовать тестовый файл")
-
+        placeholder = None
         with st.container():
             col1, col2 = st.columns((4, 1))
 
             with col1:
-                audio_file = st.file_uploader("Загрузите аудио файл", type=['wav', 'mp3', 'ogg'])
+                audio_file = st.file_uploader("Загрузите аудио файл", type=['wav', 'mp3', 'ogg', 'm4a', 'aac'])
 
                 if audio_file is not None:
                     if not os.path.exists("audio"):
@@ -168,33 +106,57 @@ def main():
                 file_details = {"Filename": audio_file.name, "FileSize": audio_file.size}
                 st.sidebar.write(file_details)
 
-            if st.button("Получить транскрипцию"):
-                # if "res" not in st.session_state:
-                #     st.session_state.update({"res": ""})
-                with st.container():
+            if st.button("Получить транскрипцию", key="transcribe"):
+                placeholder = st.empty()
+                if "transcribe" not in st.session_state.keys():
+                    st.session_state.update({"transcribe": True})
+
+                with placeholder:
                     col1, col2 = st.columns((4, 1))
+
                     with col1:
-                        # st.text("Транскрипция")
                         with st.spinner("Идет загрузка транскрипции..."):
                             if test:
-                                data = apply_punkt_to_text("raw.txt")
-                                format_for_streamlit(data)
+                                data = apply_punkt_to_text("test_text")
                             else:
                                 res = vosk_transcribe(logfile="res.txt", num=num)
-                                # if "res" in st.session_state:
-                                #     st.session_state["res"] = res
                                 data = apply_punkt_to_text("res.txt")
-                                format_for_streamlit(data)
+
+                        if editor:
+                            st.session_state.update({"edit": True})
+                            st.session_state.update({"data": data})
+                        else:
+                            st.session_state.update({"edit": False})
+                            format_for_streamlit(data)
+
+                    with col2:
+                        if not editor:
+                            show_img("images/wrd.png", width=100)
+                            st.download_button("Скачать .docx", data=data, file_name="asr.docx", key="wrd")
+                            show_img("images/txt.png", width=100)
+                            st.download_button("Скачать .txt", data=data, file_name="asr.txt", key="txt")
+
+            if editor and "edit" in st.session_state.keys():
+                placeholder = st.empty()
+                with st.container():
+                    col1, col2 = st.columns((4, 1))
+
+                    with col1:
+                        data = st.text_area("Редактировать", height=800, key="editor",
+                                            value=get_edited_text())
+                        if st.button("Сохранить изменения 💾"):
+                            st.session_state.update({"data": data})
+                            with open("edited.txt", "w", encoding="utf-8") as writer:
+                                writer.write(get_edited_text())
                     with col2:
                         # TODO: convert to .docx
+                        data = get_edited_text()
                         show_img("images/wrd.png", width=100)
-                        st.download_button("Скачать .docx", data=data)
+                        st.download_button("Скачать .docx", data=data, file_name="asr.docx", key="edit_wrd")
                         show_img("images/txt.png", width=100)
-                        st.download_button("Скачать .txt", data=data)
+                        st.download_button("Скачать .txt", data=data, file_name="asr.txt", key="edit_txt")
 
 
 if __name__ == "__main__":
-    # page settings
-    check_and_load()
-    st.set_page_config(page_title="web-app", page_icon=":vhs:", layout="wide")
+    get_weights()
     main()
