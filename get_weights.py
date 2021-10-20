ec41d15de67bfeb8480a927e756ba9dbd5f3bc04
CyberMaryVer <maria.s.startseva@gmail.com>
2021-10-20 07:19:01 +0300

    update

--- get_weights.py
+++ get_weights.py
@@ -32,7 +32,7 @@ def download_silero():
 
 
 def download_vosk(small=True):
-    os.makedirs("weights", exist_ok=True)
+    os.makedirs(f"{PATH_TO_DATA}/weights", exist_ok=True)
     # asr_model_path = VOSK_WEIGHTS[1] if small else VOSK_WEIGHTS[0]
 
     url = 'https://drive.google.com/uc?id=qmOQk0j1430iHBGzQqMT5duqGHuUJQXo'
@@ -41,7 +41,7 @@ def download_vosk(small=True):
 
     # subprocess.run(f"wget -O {PATH_TO_DATA}/model.zip {asr_model_path}")
     subprocess.run(f"unzip {PATH_TO_DATA}/model.zip -d /weights/")
-    subprocess.run("mv \"vosk-model-small-ru-0.15\" vosk-model-small-ru")
+    subprocess.run(f"mv \"vosk-model-small-ru-0.15\" vosk-model-small-ru")
 
 
 def check_and_load(verbose=True):
